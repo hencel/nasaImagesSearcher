909ce25efe746e105452b60874d76f9340d0dc18
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2021-10-07 21:57:13 +0200

    NI-002 Main structures. Form and button components

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+  @Input() placeholderText: string = '';
+  @Input() buttonText: string = '';
+  @Input() buttonClasses: string = '';
+}
