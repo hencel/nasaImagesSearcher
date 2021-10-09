dfa499e2270ac2ca8177d78ab33fa79ea1ccdcda
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2021-10-09 20:40:01 +0200

    NI-004 Submit to api text to search. Received json with datas.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,22 +1,34 @@
 import { Component, Input } from '@angular/core';
+import { ConfigService } from '../services/config.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
+  providers:  [ ConfigService ]
 })
 export class FormComponent {
   @Input() placeholderText: string = '';
   @Input() buttonText: string = '';
   @Input() buttonClasses: string = '';
 
+  constructor(private service: ConfigService) {
+    this.service = service;
+  }
+
   submitValue: string = '';
 
   clickSubmit(value: string) {
     if(value.length > 0) {
-      console.log(`Searched text: ${value}`)
+      this.submitValue = value;
+      this.submitData(value);
     }
-    
+  }
+
+  submitData(text: string):void {
+    this.service.askApi(text).subscribe((res) => {
+      console.log(res)
+    });;
   }
 
 }
