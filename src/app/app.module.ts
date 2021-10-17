7f77c82fb14f0f8db65a4a68741db7461a1e6520
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2021-10-17 11:20:55 +0200

    NI-006 Stying of displayed images. Display area as separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormComponent } from './form/form.component';
 import { ButtonComponent } from './button/button.component';
+import { DisplayAreaComponent } from './display-area/display-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     ButtonComponent,
+    DisplayAreaComponent,
   ],
   imports: [
     BrowserModule,
