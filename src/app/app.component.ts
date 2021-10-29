dffa6efc982243d2d7115cd95b274f8f5e391e31
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2021-10-29 13:12:53 +0200

    NI-008 Added link to full image to download or open in the new browser's card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,18 +37,15 @@ export class AppComponent implements OnInit {
     array.forEach((el: any) =>  {
       if(el.links && el.data[0].media_type == 'image') {
         let fullImageLink:string = '';
-        let temp: ImageItem = {preview: el.links[0].href, fullImage: fullImageLink, title: el.data[0].title};
-        tempList.push(temp)
-        // const promisedData = this.service.askJson(el.href).toPromise();
-        // promisedData.then((data)=>{
-        //   fullImageLink = data[0];
-        //   previewImageLink = data[3];
-        //   title = el.data[0].title;
-        //   let temp: ImageItem = {preview: previewImageLink , fullImage: fullImageLink, title: title};
-        //   // console.log(temp)
-        // }) 
+        const promisedData = this.service.askJson(el.href).toPromise();
+        promisedData.then((data)=>{
+          fullImageLink = data[0];
+          let temp: ImageItem = {preview: el.links[0].href, fullImage: fullImageLink, title: el.data[0].title}
+          tempList.push(temp)
+        }) 
       }
     });
+    console.log(tempList)
     return tempList;
   }
 }
